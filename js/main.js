5864e3c9336a6672d7a2df58aedc6a9c786e5690
Alex12012000 <arcex.29@gmail.com>
2022-06-06 17:06:05 +0200

    Finito

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ genBtn.addEventListener ('click',
         // Price 
         const price = userKm * 0.21;
         let discount = 0;
-        const rate = 'Tariffa Standard'
+        let rate = 'Tariffa Standard'
 
         // Discount
         if(userAge === 'minorenne'){
@@ -29,11 +29,37 @@ genBtn.addEventListener ('click',
         const ticketName = document.getElementById('name');
         const ticketRate = document.getElementById('rate');
         const ticketPrice = document.getElementById('price');
+        const ticketCarriage = document.getElementById('carriage');
+        const ticketCp = document.getElementById('cp-code');
         
         ticketName.innerHTML = userName;
         ticketRate.innerHTML = rate;
         ticketPrice.innerHTML = finalPrice.toFixed(2) + ' Euro';
+        ticketCarriage.innerHTML = Math.floor(Math.random() * 10);
+        ticketCp.innerHTML = Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000;
+
+        // Active Div
+        let ticketDiv = document.querySelector('.ticket');
+        ticketDiv.classList.add('active')
+
     }
 
 )
 
+// RETURN BUTTON
+const deleteBtn = document.getElementById('return');
+deleteBtn.addEventListener ('click', 
+    function () {
+        
+        // Delete Value
+        const userName = document.getElementById('user-name').value = '';
+        const userKm = document.getElementById('user-km').value = '';
+        const userAge = document.getElementById('user-age').value = 'maggiorenne';
+
+        // Remove div
+        let ticketDiv = document.querySelector('.ticket');
+        ticketDiv.classList.remove('active')
+
+        
+    }
+)
